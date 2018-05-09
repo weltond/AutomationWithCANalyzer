0cc6da95cb81db7e6f78d4963613c5403e32291e
hongyuan_weltond <ningh@umich.edu>
2018-05-09 12:10:15 -0400

    Update canalyzer_test.py
    This is a test script for: 1. Calling CAPL function from CANalyzer; 2. Receiving CAN signal values from CANalyzer.

--- canalyzer_test.py
+++ canalyzer_test.py
@@ -76,28 +76,6 @@ class InitiateCanalyzer:
 
     def get_can_log(self):
         return self.can_logs
-
-    def verify_no_fault_on_start(self,
-                                 signal_1="EmgcyCallFalt_B_Dsply",
-                                 message_1="TCU_Send_Signals_5"
-                                 ):
-        pythoncom.CoInitialize()
-        self.app = win32com.client.Dispatch(
-            pythoncom.CoGetInterfaceAndReleaseStream(
-                self.marshalled_app_id,
-                pythoncom.IID_IDispatch
-            )
-        )
-        print('going to verify, sleep 15 s first to wait clear DTC')
-        start_time = time.time()
-        while True:
-            if self.app.Bus.GetSignal(2, message_1, signal_1).Value == 1.0:
-                print('fault, should not start testing')
-                return False
-            elif self.app.Bus.GetSignal(2, message_1, signal_1).Value == 0.0:
-                if time.time() - start_time > 15:
-                    print('no fault, ready to start testing')
-                    return True
 	
     def get_a_signal(self):
         pythoncom.CoInitialize()
@@ -119,102 +97,60 @@ class InitiateCanalyzer:
 
             if current_status >0:
                 print current_status
+		pythoncom.CoUnInitialize()
                 break
-				
-    def get_all_signals(self, queue_from_gui, signal_enum_1, signal_1, message_1,
-                        signal_enum_2=can_signals_enum.IgnStatus,
-						signal_2="Ignition_Status", message_2="BodyInfo_3_HS4"):
 
-        pythoncom.CoInitialize()
-        self.app = win32com.client.Dispatch(
-            pythoncom.CoGetInterfaceAndReleaseStream(
-                self.marshalled_app_id,
-                pythoncom.IID_IDispatch
-            )
-        )
-
-        temp_status = -10.0
-        temp_status_2 = -10.0
-        prev_time = time.time()
-        prev_time_2 = time.time()
-        signal_delta_log_1 = list()
-        signal_delta_log_1.append("\n" + signal_enum_1.name() + " status: ")
-        # signal_delta_log_1.append(signal_enum_1.name() + " status: default -> ")
-        signal_delta_log_2 = list()
-        signal_delta_log_2.append("\n" + signal_enum_2.name() + " status: ")
-        # signal_delta_log_2.append(signal_enum_2.name() + " status: default -> ")
-        signal_current_log_1 = list()
-        signal_current_log_1.append(signal_enum_1.name() + " status: ")
-        # signal_current_log_1.append(signal_enum_1.name() + " status: default")
-        signal_current_log_2 = list()
-        signal_current_log_2.append(signal_enum_2.name() + " status: ")
-        # signal_current_log_2.append(signal_enum_2.name() + " status: default")
-        count_1 = 0
-        while True:
-            time.sleep(1)
-            current_status = self.app.Bus.GetSignal(2, message_1, signal_1).Value
-            # signal_current_log_1.append("oh " + signal_enum_1.name() + " status: " + str(current_status))
-            if temp_status != current_status:
-                count_1 += 1
-                current_time = time.time()
-                delta_time, prev_time = current_time - prev_time, current_time
-                temp_status = current_status
-                for name, member in signal_enum_1.__members__.items():
-                    if current_status == member.value:
-                        current_status = member
-                        queue_from_gui.put("curr status: {} and delta time: {} at: {}".format(
-                            current_status, delta_time, current_time))
-                        print("curr status: {} and delta time: {} at: {}".format(
-                            current_status, delta_time, current_time))
-                        signal_delta_log_1.append(str(delta_time) + " s -> " + str(current_status) + " -> " + "({})\n".format(count_1))
-                        signal_current_log_1.append(" -> " + str(current_status) + " at: " + str(current_time) + "({})\n".format(count_1))
-                        # self.can_logs.append(" -> " + str(current_status) + " at: " + str(current_time) + "\n")
-
-            current_status_2 = self.app.Bus.GetSignal(2, message_2, signal_2).Value
-            # signal_current_log_2.append("oh" + signal_enum_2.name() + " status: " + str(current_status_2))
-
-            # by default it's ign status
-            if temp_status_2 != current_status_2:
-                current_time_2 = time.time()
-                delta_time_2, prev_time_2 = current_time_2 - prev_time_2, current_time_2
-                temp_status_2 = current_status_2
-                for name, member in signal_enum_2.__members__.items():
-                    if current_status_2 == member.value:
-                        current_status_2 = member
-                        queue_from_gui.put("curr status: {} and delta time: {} at: {}".format(
-                            current_status_2, delta_time_2, current_time_2))
-                        print("curr status: {} and delta time: {} at: {}".format(
-                            current_status_2, delta_time_2, current_time_2))
-                        signal_delta_log_2.append(str(delta_time_2) + " s -> " + str(current_status_2) + " -> " + "\n")
-                        signal_current_log_2.append(" -> " + str(current_status_2) + " at: " + str(current_time_2)
-                                                    + "\n")
-                        # self.can_logs.append(" -> " + str(current_status_2) + "at: " + str(current_time_2) + "\n")
-            if self.event.wait(timeout=0.01):
-                # print("received stop: {}".format(time.ctime()))         # 4) 15:31:20
-                if not self.delta_flag:
-                    self.can_logs = self.can_logs + signal_current_log_1 + signal_current_log_2
-                    print("get current")
-                else:
-                    self.can_logs = self.can_logs + signal_delta_log_1 + signal_delta_log_2
-                    self.can_logs.append('\n')
-                    print("get delta")
-                pythoncom.CoUninitialize()
-                break
-                # print("can_logs length: {}".format(self.can_logs[0]))
+    # ========================= Interact with CAPL Function =============================
+    def select_capl_function(self, function_name1, function_name2):
+        '''
+        Initialize which CAPL functions are being selected. At most 2 functions right now.
+        :param function_name1:
+        :param function_name2:
+        :return:
+        '''
+        self.__MeasurementEvents.CAPL1 = function_name1
+        self.__MeasurementEvents.CAPL2 = function_name2
 
     def marshal_handler_1(self):
-        if self.Running():
-            call_marshal_id = pythoncom.CoMarshalInterThreadInterfaceInStream(pythoncom.IID_IDispatch,
-                                                                              self.__MeasurementEvents.CaplFunction1)
-            print 'call id:', call_marshal_id  # .__repr__
-            return call_marshal_id
+        '''
+        pass CaplFunction 1 to COM thread
+        :return:
+        '''
+        call_marshal_id = pythoncom.CoMarshalInterThreadInterfaceInStream(pythoncom.IID_IDispatch,
+                                                                          self.__MeasurementEvents.CaplFunction1)
+        return call_marshal_id
 
     def marshal_handler_2(self):
-        if self.Running():
-            call_marshal_id = pythoncom.CoMarshalInterThreadInterfaceInStream(pythoncom.IID_IDispatch,
-                                                                              self.__MeasurementEvents.CaplFunction2)
-            print 'call id:', call_marshal_id  # .__repr__
-            return call_marshal_id
+        '''
+        pass CaplFunction 2 to COM thread
+        :return:
+        '''
+        call_marshal_id = pythoncom.CoMarshalInterThreadInterfaceInStream(pythoncom.IID_IDispatch,
+                                                                          self.__MeasurementEvents.CaplFunction2)
+        return call_marshal_id
+
+    @staticmethod
+    def execute_capl_function(call_marshal_id):
+        '''
+        Execute Capl Function based on call_marshal_id
+        :param call_marshal_id: determine which Capl Function is called.
+        :return:
+        '''
+        pythoncom.CoInitialize()
+        measurement_event = pythoncom.CoGetInterfaceAndReleaseStream(
+            call_marshal_id,
+            pythoncom.IID_IDispatch
+        )
+        m = win32com.client.Dispatch(measurement_event)
+
+        print "Now trying to call CAPL func now"
+        while True:
+            pythoncom.PumpWaitingMessages()
+            ret = m.Call()
+            print "ret is {}".format(ret)
+            pythoncom.CoUninitialize()
+            return ret
+    # ========================= Interact with CAPL Function =============================
 
 
 def write_can_to_file(current_can_logs, start_time):
@@ -226,6 +162,7 @@ def write_can_to_file(current_can_logs, start_time):
         # f.write('\n\n')
         print("write finish!")
 
+	
 def main(id1, id2):
     event = threading.Event()
     listC = []
@@ -295,13 +232,15 @@ if __name__ == "__main__":
     import help_utils
     id1 = help_utils.generate_app_marshal()
     id2 = help_utils.generate_app_marshal()
-	'''
+    ''' # Test 1 for calling CAPL function.
     threading.Thread(target=main, args=(id1, id2)).start()
     l = [ '213']
     r = help_utils.get_dtc_msg_from_list(l)
 
     print r
     '''
+	
+    '''	# Test 2 for getting signal values from CANalyzer
     event = threading.Event()
     listC = []
     listD = []
@@ -313,5 +252,5 @@ if __name__ == "__main__":
     ic.start()
     threading.Thread(target=ic.get_a_signal).start()
     help_utils.wait_and_pump_msg()
-    
+    '''
 
